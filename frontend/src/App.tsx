aa6b1832b5fcf3af0a97cf7c08404daed122cd91
patrick-miharisoa <tojoniaina.patrick@futurmap.com>
2023-11-02 08:18:35 +0300

    setup react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
 import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/globalStyles.ts";
 import { theme } from "./styles/theme";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes.tsx";
 
 function App() {
-  return <ThemeProvider theme={theme}>Hello world</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
