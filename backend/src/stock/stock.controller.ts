080cbebf30bb905fec3cea08deb555997104879a
acf-patrick <miharisoap@gmail.com>
2023-11-04 18:56:24 +0300

    updated API docs

--- backend/src/stock/stock.controller.ts
+++ backend/src/stock/stock.controller.ts
@@ -14,19 +14,22 @@ import { StockService } from './stock.service';
 import {
   ApiBadRequestResponse,
   ApiOkResponse,
+  ApiOperation,
   ApiParam,
   ApiQuery,
+  ApiTags,
 } from '@nestjs/swagger';
 import { UpdateMedicineDto } from './dto/UpdateMedicine.dto';
 
+@ApiTags('Stock 📦')
 @Controller('api/stock')
 export class StockController {
   constructor(private readonly stockService: StockService) {}
 
   @Get()
   @ApiQuery({ name: 'page', description: 'Page index to fetch ' })
-  @ApiOkResponse({ description: 'Returns medicines list' })
   @ApiBadRequestResponse({ description: 'Page index overflow' })
+  @ApiOperation({ summary: 'Returns medicines list' })
   async getPage(@Query('page', ParseIntPipe) page: number) {
     const pageCount = await this.stockService.getPageCount();
     if (page >= pageCount) {
@@ -46,6 +49,7 @@ export class StockController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Returns medicine with given ID' })
   async getMedicine(@Param('id') id: string) {
     const medicine = await this.stockService.getMedicine(id);
     if (!medicine) {
@@ -56,6 +60,7 @@ export class StockController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Remove medicine with given ID' })
   async deleteMedicine(@Param('id') id: string) {
     try {
       await this.stockService.deleteMedicine(id);
@@ -66,7 +71,7 @@ export class StockController {
   }
 
   @Patch(':id')
-  @ApiOkResponse({ description: 'Medicine item successfully updated' })
+  @ApiOperation({ summary: 'Updates medicine with given ID from stock' })
   @ApiBadRequestResponse({ description: 'Invalid body format' })
   @ApiParam({ name: 'id', description: "medicine's ID" })
   async updateMedicine(
