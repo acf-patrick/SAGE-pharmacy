5c7a81e0dea0f1685aeba4051964a0f845b5ad0a
acf-patrick <miharisoap@gmail.com>
2023-11-04 18:58:48 +0300

    update

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller('/api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiExcludeEndpoint()
   getHello(): string {
     return this.appService.getHello();
   }
