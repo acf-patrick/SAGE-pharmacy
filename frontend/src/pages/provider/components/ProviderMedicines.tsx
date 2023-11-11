4a461890595c2768ea77f2a5b08b0da5337a6a93
acf-patrick <miharisoap@gmail.com>
2023-11-11 21:37:53 +0300

    setup medicine page for provider

--- frontend/src/pages/provider/components/ProviderMedicines.tsx
+++ frontend/src/pages/provider/components/ProviderMedicines.tsx
@@ -1,8 +1,17 @@
 import { useLoaderData } from "react-router-dom";
 import { Provider } from "../../../models";
+import styled from "styled-components";
+
+const StyledTitle = styled.h2`
+  color: ${({ theme }) => theme.colors.tertiary};
+`;
 
 export default function ProviderMedicines() {
   const provider = useLoaderData() as Provider;
 
-  return provider.name;
+  return (
+    <>
+      <StyledTitle>{provider.name}</StyledTitle>
+    </>
+  );
 }
