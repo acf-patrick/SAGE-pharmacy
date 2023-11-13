8b10d08ab737931f4ce097359bf9a12f80aee022
acf-patrick <miharisoap@gmail.com>
2023-11-13 19:25:44 +0300

    added acces-token strategy and guard

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [PrismaModule, UserModule],
 })
 export class AuthModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private prisma: PrismaService,
+  ) {}
+
+  // Register user and returns the ID
+  async register(name: string, password: string) {
+    try {
+      await this.userService.getUserByName(name);
+      throw new ConflictException(`Username '${name}' has already been taken`);
+    } catch {
+      const salt = await bcrypt.genSalt();
+      const hashed = await bcrypt.hash(password, salt);
+      const { id } = await this.prisma.user.create({
+        data: {
+          username: name,
+          password: hashed,
+        },
+        select: {
+          id: true,
+        },
+      });
+      return id;
+    }
+  }
+
+  // Returns new access-token and refresh-token with valid credentials, otherwise returns unauthorized exception
+  async login(name: string, password: string) {
+    try {
+      const user = await this.userService.getUserByName(name);
+
+      const correct = await bcrypt.compare(password, user.password);
+      if (correct) {
+      } else {
+        throw new UnauthorizedException('Invalid username or password');
+      }
+    } catch {
+      throw new UnauthorizedException('Invalid username or password');
+    }
+  }
+}
