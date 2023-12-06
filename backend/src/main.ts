1b9a00269d00298ed0f9365c0c8aeb3b77f6252b
acf-patrick <miharisoap@gmail.com>
2023-12-06 12:56:07 +0300

    remove mkcert from backend/package.json

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,23 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as fs from 'fs';
-import { join } from 'path';
 import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 
-const httpsOptions =
-  process.env.NODE_ENV === 'production'
-    ? {
-        key: fs.readFileSync(join(__dirname, '..', 'secrets', 'cert.key')),
-        cert: fs.readFileSync(join(__dirname, '..', 'secrets', 'cert.crt')),
-      }
-    : undefined;
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
